80f3d648a0d34d7314f84ace98e3de1b7b30b7b0
joaovitor553 <00001091567797sp@al.educacao.sp.gov.br>
2024-10-20 19:22:54 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+CriaCartão(
+    'paleontologia',
+     'Onde viveu o tiranossauro rex',
+     'Ele viveu na América do Norte, há 65 milhões de anos',
+)
+
+CriaCartão(
+    'paleontologia',
+    'Qual dinossauro é conhecido como ladrão veloz',
+    'É o velociraptor, pois seu nome em latim raptor significa ladrão',
+)
+
+CriaCartão(
+    'paleontologia',
+    'Qual foi o maior dinossauro brasileiro já descoberto',
+    'Foi o Uberatitan ribeori',
+)
+
+CriaCartão(
+    'paleontologia',
+    'Qual característica do carnotauro se destacava dos outros carnivoros',
+    'Ele possuia dois chifres na cabeça semelhante dos touros',
+)
